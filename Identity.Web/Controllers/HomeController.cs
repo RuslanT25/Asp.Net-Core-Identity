1cf76c1c91385c20d94ded398b484790216277a6
Ruslan <tagizaderuslan25@gmail.com>
2024-06-27 15:21:22 +0400

    Added Identity package,SignUp model and view.

--- Identity.Web/Controllers/HomeController.cs
+++ Identity.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-using Identity.Web.Models;
+﻿using Identity.Web.Models;
+using Identity.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace Identity.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -20,6 +24,34 @@ namespace Identity.Web.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpVM request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var indentityResult = await _userManager.CreateAsync(new() { UserName = request.UserName, PhoneNumber = request.Phone, Email = request.Email }, request.ConfirmPassword);
+
+            if (indentityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Successfully logged in.";
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            foreach (IdentityError item in indentityResult.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
             return View();
         }
     }
---
+++ Identity.Web/ViewModels/SignUpVM.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Web.ViewModels
+{
+    public class SignUpVM
+    {
+        public SignUpVM()
+        {
+            
+        }
+
+        public SignUpVM(string userName, string email, string password, string phone, string confirmPassword)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            Phone = phone;
+            ConfirmPassword = confirmPassword;
+        }
+
+        [Required(ErrorMessage = "Username can't be empty.")]
+        [Display(Name = "Username :")]
+        public string UserName { get; set; } = null!;
+
+        [EmailAddress(ErrorMessage = "Email format is incorrect.")]
+        [Required(ErrorMessage = "Email can't be empty.")]
+        [Display(Name = "Email :")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone can't be empty.")]
+        [Display(Name = "Phone :")]
+        public string Phone { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password can't be empty.")]
+        [Display(Name = "Password :")]
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password isn't same.")]
+        [Required(ErrorMessage = "Confirm password can't be empty")]
+        [Display(Name = "Confirm password :")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
